ccd5f4db12b6dc56a5c585d6225a140cd272092d
Kiemama Hama <kiemamahama@gmail.com>
2026-07-24 01:06:50 +0000

    Fallback DATABASE_URL pour éviter erreur au build

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,9 +3,11 @@ import { defineConfig, env } from "prisma/config";
 
 dotenv.config({ override: true });
 
+const databaseUrl = process.env.DATABASE_URL || "file:./dev.db";
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: env("DATABASE_URL"),
+    url: databaseUrl,
   },
 });
